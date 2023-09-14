71d88a9b6592aa87b68342c35398c606c79a67e8
CodeLord36 <93430616+CodeLord36@users.noreply.github.com>
2023-09-14 16:45:19 +0100

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWindows } from "@fortawesome/free-brands-svg-icons"
-import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { faWindows } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
